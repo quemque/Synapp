cd3e85e6798225df4665151a8ffe532148dfaeff
que <kosstetz@gmail.com>
2025-08-20 16:42:18 +0300

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import TaskForm from "./components/TaskForm";
 import TaskField from "./components/TaskField";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const savedTasks = localStorage.getItem("taskfield");
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
   const handleAddTask = (newTaskText) => {
     const newTask = {
       id: Date.now(),
       text: newTaskText,
       completed: false,
     };
-    setTasks([...tasks, newTask]);
+
+    setTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks, newTask];
+      localStorage.setItem("taskfield", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
   };
+
   const handleDeleteById = (id) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.filter((task) => task.id !== id);
+      localStorage.setItem("taskfield", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
   };
+
   const handleToggleComplete = (id) => {
-    setTasks((prevTasks) =>
-      prevTasks.map((task) =>
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task
-      )
-    );
+      );
+      localStorage.setItem("taskfield", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
   };
+
   return (
     <div>
       <h1 className="text-capital">Todo app</h1>
