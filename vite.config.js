b975655980ebb329792a9944efbfbf6ec0847859
que <kosstetz@gmail.com>
2025-08-28 04:04:38 +0300

    version for vercel

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
+});
