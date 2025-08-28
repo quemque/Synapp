5e78b52ea147d380559d14a2be3c3bea15ec04eb
que <kosstetz@gmail.com>
2025-08-28 23:50:10 +0300

    api test

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const API_URL = "https://todo-react-production-603e.up.railway.app";
+//const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 export const authAPI = {
   login: async (email, password) => {
-    console.log("Environment variables:", import.meta.env);
-    console.log("API_URL:", import.meta.env.VITE_API_URL);
     const response = await fetch(`${API_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -13,8 +12,6 @@ export const authAPI = {
   },
 
   register: async (username, email, password) => {
-    console.log("Environment variables:", import.meta.env);
-    console.log("API_URL:", import.meta.env.VITE_API_URL);
     const response = await fetch(`${API_URL}/api/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
