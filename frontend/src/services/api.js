1a48310e2de6385fcd323ff7b57fbc47f5143db3
que <kosstetz@gmail.com>
2025-08-28 23:41:23 +0300

    api logs for vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,8 @@ const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 export const authAPI = {
   login: async (email, password) => {
+    console.log("Environment variables:", import.meta.env);
+    console.log("API_URL:", import.meta.env.VITE_API_URL);
     const response = await fetch(`${API_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -11,6 +13,8 @@ export const authAPI = {
   },
 
   register: async (username, email, password) => {
+    console.log("Environment variables:", import.meta.env);
+    console.log("API_URL:", import.meta.env.VITE_API_URL);
     const response = await fetch(`${API_URL}/api/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
