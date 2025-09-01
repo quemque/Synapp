b7b8ffc7b88335b01c7035c8da1063e96b6cd9fa
que <kosstetz@gmail.com>
2025-09-01 22:17:52 +0300

    add _VITE prefix to variables

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 
-const API_URL = import.meta.env.BACK_URL || "http://localhost:3001";
+const API_URL = import.meta.env.VITE_BACK_URL || "http://localhost:3001";
 
 const AuthContext = createContext();
 
